70f0795422f9915ea9d80032efa110a358b993b2
npulliam <nathanpulliam@gmail.com>
2021-04-14 13:54:31 -0700

    Initial

---
+++ UpdateAndDelete/src/test/java/com/nathanpulliam/updateanddelete/UpdateAndDeleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathanpulliam.updateanddelete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdateAndDeleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
